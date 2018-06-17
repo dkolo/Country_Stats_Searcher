5a94653eb92c2f902209a828e1d9fb5346442931
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-17 20:09:34 +0200

    Add searchCountries function in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+  	var countryName = $('#country-name').val();
+  	if(!countryName.length) countryName = 'Poland';
+  	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
