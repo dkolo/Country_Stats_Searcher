3b10b80749c7a101100e8dc8a018d4e8cc69c55c
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-18 17:00:35 +0200

    Change the  presentation of search result from list to table

--- js/script.js
+++ js/script.js
@@ -16,5 +16,12 @@ function searchCountries() {
 function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function(item) {
-        $('<li>').text(item.name).appendTo(countriesList);
+    	$('<table>').append($('<tr>')).append($('<td>').text('Kraj ')).append($('<td>').text(item.name))
+    	.append($('<tr>')).append($('<td>').text('Pełna nazwa ')).append($('<td>').text(item.altSpellings))
+    	.append($('<tr>')).append($('<td>').text('Nazwa regionu ')).append($('<td>').text(item.subregion))
+    	.append($('<tr>')).append($('<td>').text('Waluta ')).append($('<td>').text(item.currencies))
+    	.append($('<tr>')).append($('<td>').text('Zajmowana powierzchnia ')).append($('<td>').text(item.area))
+    	.append($('<tr>')).append($('<td>').text('Liczba mieszkańców ')).append($('<td>').text(item.population))
+        .appendTo(countriesList);
     });
+}
